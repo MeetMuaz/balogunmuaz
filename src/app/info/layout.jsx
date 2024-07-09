19f75cb4eeafed64c2e62b41ed7375e32efc2c84
MeetMuaz <muazbalogun97@gmail.com>
2024-07-09 16:23:21 +0100

    hshhfhf

--- src/app/info/layout.jsx
+++ src/app/info/layout.jsx
@@ -1,8 +1,6 @@
 import { Inter } from "next/font/google";
 import "../globals.css";
 import Head from "next/head";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/info/page.jsx
+++ src/app/info/page.jsx
@@ -6,26 +6,6 @@ import AboutMe from "../component/AboutMe";
 import GridContainer from "../component/GridContainer";
 import ContentCard1 from "../component/ContentCard1";
 import ContentCard2 from "../component/ContentCard2";
-import TechStack from "../component/TechStack";
-
-import { FaDocker, FaAws, FaReact, FaNode, FaPhp, FaJava } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io5";
-import { DiPostgresql } from "react-icons/di";
-import { DiMysql } from "react-icons/di";
-import { DiMongodb } from "react-icons/di";
-
-const techstacks = [
-  { logo: FaDocker },
-  { logo: FaAws },
-  { logo: FaReact },
-  { logo: FaNode },
-  { logo: FaPhp },
-  { logo: FaJava },
-  { logo: IoLogoJavascript },
-  { logo: DiPostgresql },
-  { logo: DiMysql },
-  { logo: DiMongodb }
-];
 
 export default function Home() {
   return (
@@ -38,7 +18,6 @@ export default function Home() {
         <ContentCard2 title={""} content={""} />
         <ContentCard1 title={""} content={""} />
       </GridContainer>
-      <TechStack techstacks={techstacks} />
       <Footer />
     </main>
   );
